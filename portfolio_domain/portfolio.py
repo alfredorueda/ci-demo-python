1d72f1eaa0f509b71f04c9069e37abd5148220f0
Alfredo Rueda <alfredoru@gmail.com>
2026-08-05 14:24:31 +0530

    Introduce a bug on purpose for the CI demo

--- portfolio_domain/portfolio.py
+++ portfolio_domain/portfolio.py
@@ -53,7 +53,7 @@ class Portfolio:
             )
 
         lots = self._lots[ticker]
-        remaining_to_sell = quantity
+        remaining_to_sell = 0
         cost_basis = Money.of(0)
 
         while remaining_to_sell > 0:
