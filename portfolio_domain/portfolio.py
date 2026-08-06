04cdef3b6ad380921ab2e308fde79f0d56dbe32b
Alfredo Rueda <alfredoru@gmail.com>
2026-08-06 09:35:40 +0530

    Fix the cash balance bug

--- portfolio_domain/portfolio.py
+++ portfolio_domain/portfolio.py
@@ -41,7 +41,8 @@ class Portfolio:
             raise InsufficientFundsError(
                 f"Cannot buy {quantity} {ticker}: cost {cost} exceeds cash balance {self._cash}"
             )
-        #self._cash = self._cash.subtract(cost)
+        self._cash = self._cash.subtract(cost)
+        #Ok I'm adding some nice code to this feature
         self._lots.setdefault(ticker, deque()).append(_Lot(quantity, price))
 
     def sell(self, ticker: str, quantity: int, price: Money) -> Money:
